904c07e780944877d54e6d0b24dfaf20a82eb1a4
dcechano <dcechano@emich.edu>
2025-05-16 15:00:04 -0400

    impl States struct to replace vec

--- src/blox/state.rs
+++ src/blox/state.rs
@@ -1,10 +1,11 @@
 use serde::{Deserialize, Serialize};
 
-use super::enums::EnumDef;
+use super::enums::{EnumDef, EnumVariant};
 
-#[derive(Default, Serialize, Deserialize, Eq, PartialEq, Debug)]
-#[serde(rename = "stateenum")]
-pub struct StateEnum(EnumDef);
+// Wrapper for a state enum definition
+#[derive(Serialize, Deserialize, Eq, PartialEq, Debug)]
+#[serde(rename = "state_enum")]
+pub struct StateEnum(pub EnumDef);
 
 impl StateEnum {
     pub fn new(enum_def: EnumDef) -> Self {
@@ -16,21 +17,81 @@ impl StateEnum {
     }
 }
 
+// Individual state definition
 #[derive(Serialize, Deserialize, Eq, PartialEq, Debug)]
 #[serde(rename = "state")]
 pub struct State {
     pub ident: String,
-    pub parent: Option<Box<State>>,
+    pub parent: Option<String>,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub variants: Option<Vec<EnumVariant>>,
 }
 
 impl State {
-    pub fn new<S>(ident: S) -> Self
+    // Simple constructor with all fields
+    pub fn new<S>(ident: S, parent: Option<String>, variants: Option<Vec<EnumVariant>>) -> Self
     where
         S: Into<String>,
     {
         Self {
             ident: ident.into(),
-            parent: None,
+            parent,
+            variants,
         }
+    }
+}
+
+impl From<&str> for State {
+    fn from(ident: &str) -> Self {
+        Self::new(ident, None, None)
+    }
+}
+
+// Container for all states and state enum
+#[derive(Serialize, Deserialize, Eq, PartialEq, Debug)]
+pub struct States {
+    pub state_enum: StateEnum,
+    pub states: Vec<State>,
+}
+
+impl States {
+    // Create a new States container with explicit state_enum
+    pub fn new(states: Vec<State>, state_enum: StateEnum) -> Self {
+        Self { state_enum, states }
+    }
+
+    // Get a state by name
+    pub fn get_state(&self, name: &str) -> Option<&State> {
+        self.states.iter().find(|s| s.ident == name)
+    }
+
+    // Validate that all state references point to existing states
+    pub fn validate(&self) -> Result<(), String> {
+        for state in &self.states {
+            if let Some(parent) = &state.parent {
+                if !self.states.iter().any(|s| &s.ident == parent) {
+                    return Err(format!(
+                        "State '{}' has unknown parent '{}'",
+                        state.ident, parent
+                    ));
+                }
+            }
+        }
+
+        // Validate enum variants
+        for variant in &self.state_enum.get().variants {
+            for arg in &variant.args {
+                // Try to check if the argument references a state
+                let arg_str = arg.to_string();
+                if !arg_str.contains("::") && !self.states.iter().any(|s| s.ident == arg_str) {
+                    return Err(format!(
+                        "Variant '{}' references unknown state '{}'",
+                        variant.ident, arg_str
+                    ));
+                }
+            }
+        }
+
+        Ok(())
     }
 }
